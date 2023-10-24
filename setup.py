363cd99e9aadc719c50e3861c4174ac9d70b3dd1
Didictateur <decosse.adrien@gmail.com>
2023-10-24 16:19:13 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="QElephant",
-    version="1.0.",
+    version="1.0.6",
     description="A small python module simulating a quantum computer",
     author="Didictateur",
     author_email="decosse.adrien@gmail.com",
